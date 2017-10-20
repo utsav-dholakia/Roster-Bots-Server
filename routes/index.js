3e8c2faf98fe9af60e59f9113734293ab1522656
Utsav Dholakia <uvd1992@gmail.com>
2017-10-19 21:58:39 -0500

    Created roster with player names, total score and separate skill points

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ const teamSize = 15;
 const salaryCap = 175;
 
 //Generate team players and return roster
-router.get('/generateTeam', function(req, res) {
+router.post('/generateTeam', function(req, res) {
 
     //Get team name from user and generate player name with that prefix
     var teamName = req.body.teamName;
@@ -17,22 +17,22 @@ router.get('/generateTeam', function(req, res) {
     if(!/^[a-z]+$/i.test(teamName)){
         teamName = 'ABCD';
     }
-    var totalScore = generateRoster();
-
-
+    generateTotalScores(teamName, function(roster){
+        res.send(roster);
+    });
 });
 
-function generateRoster(){
+var generateTotalScores = function(teamName, callBack){
     //3 sets of values possible for "total attribute score" for each player
     const set1Min = 4, set1Max = 15;
     const set2Min = 16, set2Max = 25;
     const set3Min = 26, set3Max = 100;
 
     //Probability of each set above
-    var probability1 = 68, probability2 = 98, probability3 = 2;
+    var probability1 = 68, probability2 = 98;
 
     //Array containing total score values of all players
-    var totalScore = [];
+    var totalScores = [];
     //count variable keeps track of whether to change the probability of sets above or not
     var count = (Math.ceil(salaryCap / teamSize) * teamSize) - salaryCap;
     var totalAttributeScore = 0;
@@ -44,24 +44,24 @@ function generateRoster(){
         //Generate scoreValue from sets above if the value is between probability limits defined above
         if(value <= probability1){
             scoreValue = Math.floor((Math.random() * (set1Max - set1Min + 1)) + set1Min);
-            while(totalScore.includes(scoreValue)){
+            while(totalScores.includes(scoreValue)){
                 scoreValue = Math.floor((Math.random() * (set1Max - set1Min + 1)) + set1Min);
             }
-            totalScore.push(scoreValue);
+            totalScores.push(scoreValue);
         }
         else if(value <= probability2){
             scoreValue = Math.floor((Math.random() * (set2Max - set2Min + 1)) + set2Min);
-            while(totalScore.includes(scoreValue)){
+            while(totalScores.includes(scoreValue)){
                 scoreValue = Math.floor((Math.random() * (set2Max - set2Min + 1)) + set2Min);
             }
-            totalScore.push(scoreValue);
+            totalScores.push(scoreValue);
         }
         else{
             scoreValue = Math.floor((Math.random() * (set3Max - set3Min + 1)) + set3Min);
-            while(totalScore.includes(scoreValue)){
+            while(totalScores.includes(scoreValue)){
                 scoreValue = Math.floor((Math.random() * (set3Max - set3Min + 1)) + set3Min);
             }
-            totalScore.push(scoreValue);
+            totalScores.push(scoreValue);
         }
 
         //Count total attribute sum of all generated players till now
@@ -72,7 +72,7 @@ function generateRoster(){
             i = -1;
             totalAttributeScore = 0;
             count = (Math.ceil(salaryCap / teamSize) * teamSize) - salaryCap;
-            totalScore = [];
+            totalScores = [];
             continue;
         }
         //Change value of count accordingly
@@ -87,7 +87,32 @@ function generateRoster(){
             probability1 = 68;
         }
     }
-    return totalScore;
+    generatePlayerSkills(teamName, totalScores, callBack);
+}
+
+var generatePlayerSkills = function(teamName, totalScores, callBack){
+    var roster = [];
+
+    for(var i = 0; i < totalScores.length; i++){
+        var player = {};
+        //Generate player name in a sequence of alphanumeric values
+        player.Name = teamName + '123' + (i+1);
+
+        //Generate player's skill values from total score
+        player.totalScore = totalScores[i];
+        //First skill point can be from 0 to totalScore point
+        player.Speed = Math.floor(Math.random() * (player.totalScore));
+
+        //Second skill point can be from 0 to totalScore - firstSkill point
+        player.Strength = Math.floor(Math.random() * ((player.totalScore - player.Speed)));
+
+        //Third skill point will be totalScore - firstSkill - secondSkill point
+        player.Agility = player.totalScore - player.Strength - player.Speed;
+
+        roster.push(player)
+    }
+
+    callBack(roster);
 }
 
 module.exports = router;
